a745214114a8612d63cd07e2a7959d07af690907
Jaowzinho <157393407+Jaowzinho@users.noreply.github.com>
2025-05-09 08:50:39 -0300

    Add files via upload

---
+++ camisa_time.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS camisa_time;
+
+CREATE DATABASE camisa_time;
+
+USE camisa_time;
+
+CREATE TABLE camisas (
+    id_camisa INT AUTO_INCREMENT PRIMARY KEY,
+    nome_camisa VARCHAR(100) NOT NULL,
+    ano_camisa YEAR NOT NULL,
+    tamanho_camisa ENUM('PP', 'P', 'M', 'G', 'GG', 'XG') NOT NULL,
+    preco_camisa DECIMAL(10, 2) NOT NULL,
+    url_img VARCHAR(255) NOT NULL
+);
