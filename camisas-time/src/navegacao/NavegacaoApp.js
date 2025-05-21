bf2736ea4a32b40416ec1ab4b8fb36cf9715e90e
Lucas <lucasnicos2016@gmail.com>
2025-05-21 11:12:43 -0300

    Update NavegacaoApp.js

--- camisas-time/src/navegacao/NavegacaoApp.js
+++ camisas-time/src/navegacao/NavegacaoApp.js
@@ -2,8 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TelaInicial from "../telas/TelaInicial";
-import TelaAdicionar from "../telas/TelaAdicionar";
-import TelaEditar from "../telas/TelaEditar";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,16 +13,6 @@ export default function NavegacaoApp() {
           name="Camisas"
           component={TelaInicial}
           options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Editar"
-          component={TelaEditar}
-          options={{ title: "Editar Camisa" }}
-        />
-        <Stack.Screen
-          name="Adicionar"
-          component={TelaAdicionar}
-          options={{ title: "Adicionar Camisa" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
